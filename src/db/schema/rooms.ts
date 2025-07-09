2c3f0603862c3cc4287098fdef7cd5747285f5ba
show <39679130+showbr@users.noreply.github.com>
2025-07-09 15:02:03 -0300

    feat: add create question, create room and get room questions

--- src/db/schema/rooms.ts
+++ src/db/schema/rooms.ts
@@ -3,6 +3,6 @@ import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 export const rooms = pgTable('rooms', {
 	id: uuid().primaryKey().defaultRandom(),
 	name: text().notNull(),
-	descricao: text(),
+	description: text(),
 	createdAt: timestamp().defaultNow().notNull(),
 });
